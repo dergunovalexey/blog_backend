3f4b7538d4fd85b472ce83775823d326ef6ee123
dergunovalexey <dergunovalexey@mail.ru>
2018-08-06 02:24:27 +0700

    update create into blogentryserializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -9,7 +9,7 @@ class BlogEntryFileSerializer(serializers.ModelSerializer):
 
 
 class BlogEntrySerializer(serializers.ModelSerializer):
-    files = BlogEntryFileSerializer(many=True, required=True)
+    files = BlogEntryFileSerializer(many=True, required=False)
 
     class Meta:
         model = BlogEntry
@@ -17,6 +17,20 @@ class BlogEntrySerializer(serializers.ModelSerializer):
                   'description', 'likes', 'created_at', 'files')
         read_only_fields = ('id', 'author', 'preview', 'description',
                             'likes', 'created_at')
+
+    def create(self, validated_data):
+        files = validated_data.pop('files', [])
+        instance = super().create(validated_data)
+        obj_list = []
+        for f in files:
+            obj_list.append(BlogEntryFile(
+                blog_entry_id=instance['id'],
+                file=f
+            ))
+
+        if obj_list:
+            BlogEntryFile.objects.bulk_create(obj_list, 100)
+        return instance
 
 
 class BlogEntryListSerializer(serializers.ModelSerializer):
