7378d312b9ddcc71f24e9768f09a04687cb43643
dergunovalexey <dergunovalexey@mail.ru>
2018-08-05 21:57:43 +0700

    add previewset to blogentry

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import (CreateModelMixin, ListModelMixin,
+                                   RetrieveModelMixin, DestroyModelMixin)
 
-# Create your views here.
+from blog.models import BlogEntry
+
+
+class BlogEntryViewSet(CreateModelMixin, ListModelMixin, RetrieveModelMixin,
+                       DestroyModelMixin, GenericViewSet):
+    queryset = BlogEntry.objects.all().order_by('-created_at')
