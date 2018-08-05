07051578daacf306cdb90c8b31acc69c1b0ebf33
dergunovalexey <dergunovalexey@mail.ru>
2018-08-05 22:06:48 +0700

    add paginator to blogentryviewset

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,10 @@ from rest_framework.mixins import (CreateModelMixin, ListModelMixin,
                                    RetrieveModelMixin, DestroyModelMixin)
 
 from blog.models import BlogEntry
+from blog.paginations import BlogEntryPagination
 
 
 class BlogEntryViewSet(CreateModelMixin, ListModelMixin, RetrieveModelMixin,
                        DestroyModelMixin, GenericViewSet):
     queryset = BlogEntry.objects.all().order_by('-created_at')
+    pagination_class = BlogEntryPagination
