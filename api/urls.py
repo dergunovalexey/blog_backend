86a1f81a59dbc75e22b72cb4cafefab8e5c02b17
dergunovalexey <dergunovalexey@mail.ru>
2018-08-06 00:03:18 +0700

    add authentication url

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework_swagger.views import get_swagger_view
+from rest_framework.authtoken import views
+
 
 schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
     url(r'^blog/', include('blog.urls')),
     url(r'^doc/', schema_view),
+    url(r'^token-auth/', views.obtain_auth_token)
 ]
