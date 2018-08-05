d2c07e44d7a50a521967df5ab8a3aa69f55d254e
dergunovalexey <dergunovalexey@mail.ru>
2018-08-05 23:54:14 +0700

    register blogentry to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from blog.models import BlogEntry, BlogEntryFile
 
-# Register your models here.
+
+class BlogEntryFileInline(admin.TabularInline):
+    model = BlogEntryFile
+
+
+@admin.register(BlogEntry)
+class BlogEntryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'title', 'created_at')
+    list_filter = ('created_at', )
+    search_fields = ('id', 'author__username', 'title', 'body')
+    ordering = ('-created_at', )
+    inlines = (BlogEntryFileInline, )
