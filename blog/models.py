8be5ee89d198a768cb00e1c92ce222e02cbfb8da
dergunovalexey <dergunovalexey@mail.ru>
2018-08-06 02:04:08 +0700

    update field preview into blogentry

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from blog.tasks import get_preview_description
 
 
 class BlogEntry(models.Model):
@@ -7,8 +8,7 @@ class BlogEntry(models.Model):
     title = models.CharField(max_length=128)
     body = models.TextField()
     link = models.URLField(blank=True, null=True)
-    preview = models.ImageField(upload_to='blog/blog_entry/preview/',
-                                blank=True, null=True)
+    preview = models.URLField(blank=True, null=True)
     description = models.TextField(blank=True)
     likes = ArrayField(models.IntegerField(), default=list, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -17,8 +17,13 @@ class BlogEntry(models.Model):
         return self.title
 
     def save(self, *args, **kwargs):
+        # delete duplicate elements
         self.likes = list(set(self.likes))
+
         super().save(*args, **kwargs)
+
+        # get description and preview from link
+        get_preview_description.delay(self.id)
 
 
 class BlogEntryFile(models.Model):
