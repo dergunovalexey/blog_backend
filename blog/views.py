b9abae6955b91049ba5397d8b0a7c9040d2be493
dergunovalexey <dergunovalexey@mail.ru>
2018-08-05 22:25:34 +0700

    add author to serializer save

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,14 @@ from rest_framework.mixins import (CreateModelMixin, ListModelMixin,
 
 from blog.models import BlogEntry
 from blog.paginations import BlogEntryPagination
+from blog.serializers import BlogEntrySerializer
 
 
 class BlogEntryViewSet(CreateModelMixin, ListModelMixin, RetrieveModelMixin,
                        DestroyModelMixin, GenericViewSet):
     queryset = BlogEntry.objects.all().order_by('-created_at')
     pagination_class = BlogEntryPagination
+    serializer_class = BlogEntrySerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=serializer.context['request'].user)
