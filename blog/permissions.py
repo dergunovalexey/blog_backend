0d1459247d0443fc8482429996aaa9611c81021a
Дергунов Алексей <dergunovalexey@mail.ru>
2018-08-13 13:18:00 +0700

    optim custom permission

--- blog/permissions.py
+++ blog/permissions.py
@@ -2,10 +2,10 @@ from rest_framework import permissions
 
 
 class BlogEntryPermission(permissions.BasePermission):
-    message = 'you not owner this entry'
+    message = 'you are not owner this entry'
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.author == request.user
+        return obj.author_id == request.user.id
